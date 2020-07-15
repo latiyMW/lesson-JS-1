4d7ea96ff663919c255767490f774b9684785d46
MikhailLatii <trali_vali@mail.ru>
2020-07-15 19:39:46 +0300

    first commit

---
+++ Lesson-JS-1.js
@@ -0,0 +1,15 @@
+// Конвертация температур
+let celsus = 15;
+let farengeit = (9/5) * celsus + 32 + ' deg';
+alert(farengeit);
+
+// Работа с переменными
+let admin;
+let name;
+name = 'Василий';
+admin = name;
+alert(admin);
+
+// Конкатенация
+let question = 1000 + '108';
+alert(question);
